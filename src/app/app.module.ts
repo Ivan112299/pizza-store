c8b8132177299989daac432e3180380b56f18406
ikozin <kozinvanya@mail.ru>
2024-03-21 19:39:31 +0300

    add responsive for header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './shared/components/menu/menu.component';
-import { AboutComponent } from './home/components/about/about.component';
-import { PizzaCardComponent } from './shared/components/pizza-card/pizza-card.component';
-import { CallToActionComponent } from './home/components/call-to-action/call-to-action.component';
-import { PizzaService } from './shared/services/pizza-data.service';
 import { DrinksComponent } from './drinks/components/drinks/drinks.component';
 import { DeliveryComponent } from './delivery/components/delivery/delivery.component';
 import { ContactsComponent } from './contacts/components/contacts/contacts.component';
 import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
-import { PizzaItemsComponent } from './home/components/pizza-items/pizza-items.component';
 import { HomeModule } from './home/home.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { AlertComponent } from './shared/components/alert/alert.component';
 
 @NgModule({
   declarations: [
