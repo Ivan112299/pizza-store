e102110e5d483c2a7b33560438d34a10674d0da8
ikozin <kozinvanya@mail.ru>
2024-03-21 23:45:55 +0300

    fix asset path

--- src/app/shared/services/pizza-data.service.ts
+++ src/app/shared/services/pizza-data.service.ts
@@ -8,44 +8,44 @@ export class PizzaService {
     {
       name: 'Мясная де люкс',
       content: 'Пепперони, лук, бекон, томатная паста, колбаски, перец, грибы,',
-      img: './../../../../assets/mock-pizza/pizza_1.png',
+      img: 'assets/mock-pizza/pizza_1.png',
     },
     {
       name: 'Морская Премиум',
       content: 'Перец, сыр, креветки, кальмары, мидии, лосось',
-      img: './../../../../assets/mock-pizza/pizza_2.png',
+      img: 'assets/mock-pizza/pizza_2.png',
     },
     {
       name: 'Бекон и Сосиски',
       content: 'Бекон, сыр, сосиски, ананас, томатная паста',
-      img: './../../../../assets/mock-pizza/pizza_3.png',
+      img: 'assets/mock-pizza/pizza_3.png',
     },
     {
       name: 'Куриная Делюкс',
       content: 'Курица, ананас, сыр Пепперони, соус для пиццы, томатная паста',
-      img: './../../../../assets/mock-pizza/pizza_4.png',
+      img: 'assets/mock-pizza/pizza_4.png',
     },
     {
       name: 'Барбекю Премиум',
       content:
         'Свинина BBQ, соус Барбкею, сыр, курица, соус для пиццы, соус чи',
-      img: './../../../../assets/mock-pizza/pizza_4.png',
+      img: 'assets/mock-pizza/pizza_4.png',
     },
     {
       name: 'Пепперони Дабл',
       content: 'Пепперони, сыр, колбаса 2 видов: обжаренная и вареная',
-      img: './../../../../assets/mock-pizza/pizza_3.png',
+      img: 'assets/mock-pizza/pizza_3.png',
     },
     {
       name: 'Куриное трио',
       content:
         'Жареная курица, Тушеная курица, Куриные наггетсы, перец, сыр, г',
-      img: './../../../../assets/mock-pizza/pizza_2.png',
+      img: 'assets/mock-pizza/pizza_2.png',
     },
     {
       name: 'Сырная',
       content: 'Сыр Джюгас, Сыр с плесенью, Сыр Моцарелла, Сыр секретный',
-      img: './../../../../assets/mock-pizza/pizza_1.png',
+      img: 'assets/mock-pizza/pizza_1.png',
     },
   ];
 
